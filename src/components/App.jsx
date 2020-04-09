94814dfd0d457e7bf28643e573c350cd683f924c
Adam Kecskes <adam@Macs-MacBook-Pro-2.local>
2020-04-08 22:07:47 -0500

    added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import CreateArea from './CreateArea';
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
